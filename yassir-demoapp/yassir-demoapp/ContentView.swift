b236136c9ab7d235f45cd66d0df6181c95b1db4c
Vladimir <lamaxur@gmail.com>
2023-10-26 16:06:20 +0300

    init

---
+++ yassir-demoapp/yassir-demoapp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  yassir-demoapp
+//
+//  Created by Vladimir Stasenko on 26.10.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ yassir-demoapp/yassir-demoapp/yassir_demoappApp.swift
@@ -0,0 +1,17 @@
+//
+//  yassir_demoappApp.swift
+//  yassir-demoapp
+//
+//  Created by Vladimir Stasenko on 26.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct yassir_demoappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
