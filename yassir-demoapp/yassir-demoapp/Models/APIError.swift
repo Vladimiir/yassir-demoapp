ee2b95d8ada9fef2cf658606d9fe09b69919f15c
Vladimir <lamaxur@gmail.com>
2024-02-08 20:59:29 +0400

    added APIError

---
+++ yassir-demoapp/yassir-demoapp/Models/APIError.swift
@@ -0,0 +1,17 @@
+//
+//  APIError.swift
+//  yassir-demoapp
+//
+//  Created by Vladimir Stasenko on 06.02.2024.
+//
+
+import Foundation
+
+enum APIError: Error {
+    case noInternet
+    case notAuthorized
+}
+
+// general api errors
+// specific api error
+// third party api errors
